e5dbbe0df2f0a8f1ab32e057e52b4abe1a0ce3f4
Faizan Malik <malickfaizann@gmail.com>
2023-07-06 20:26:02 +0500

    Added pdf reader

---
+++ src/constants/menu_data.js
@@ -0,0 +1,10 @@
+
+export const menuData = {
+    'CONTINENTAL MENU': {link: "https://firebasestorage.googleapis.com/v0/b/pdf-uploader-efebf.appspot.com/o/files%2FhvMuKpJlkTMLzaH0WDCLfps3RbL2%2FOriental%20Menu.pdf?alt=media&token=c1e4d3db-e2d3-4f1f-8d9d-fb0ccc00ab69", title: 'CONTINENTAL MENU', imageSrc: '/variety_small.jpg'},
+    'VIP DRINKS MENU': {link: "https://firebasestorage.googleapis.com/v0/b/pdf-uploader-efebf.appspot.com/o/files%2FhvMuKpJlkTMLzaH0WDCLfps3RbL2%2FVIP%20Drink%20Menu.pdf?alt=media&token=d9a8aa8b-ebea-4ccf-a269-7d4f4d2a61d0", title: 'VIP DRINKS MENU', imageSrc: '/drinksvip.jpg'},
+    'DRINKS MENU': {link: "https://firebasestorage.googleapis.com/v0/b/pdf-uploader-efebf.appspot.com/o/files%2FhvMuKpJlkTMLzaH0WDCLfps3RbL2%2FDrink%20Menu.pdf?alt=media&token=92388818-4e30-4342-af20-25a1892ad172", title: 'DRINKS MENU', imageSrc: '/drink.jpg'},
+    'CHINESE FOOD MENU': {link: "https://firebasestorage.googleapis.com/v0/b/pdf-uploader-efebf.appspot.com/o/files%2FhvMuKpJlkTMLzaH0WDCLfps3RbL2%2FChinese%20Menu.pdf?alt=media&token=63233e34-5aa6-45d0-baf1-aefaded8a4a2", title: 'CHINESE FOOD MENU', imageSrc: '/chinese.jpg'},
+    'INDIAN FOOD MENU': {link: "https://firebasestorage.googleapis.com/v0/b/pdf-uploader-efebf.appspot.com/o/files%2FhvMuKpJlkTMLzaH0WDCLfps3RbL2%2FIndian%20Menu.pdf?alt=media&token=6bc7e2b8-1ad7-41f8-87dd-120690c1a082", title: 'INDIAN FOOD MENU', imageSrc: '/indian.jpg'},
+    'ROOM SERVICE MENU': {link: "https://firebasestorage.googleapis.com/v0/b/pdf-uploader-efebf.appspot.com/o/files%2FhvMuKpJlkTMLzaH0WDCLfps3RbL2%2FRoom%20Service.pdf?alt=media&token=a5e15b45-ba5d-4a17-8eac-a11d2badc4ce", title: 'ROOM SERVICE MENU', imageSrc: '/roomservice.png'},
+    'OTHER (COMING SOON)': {link: "#", title: 'OTHER (COMING SOON)', imageSrc: '/other.jpg'},
+};
